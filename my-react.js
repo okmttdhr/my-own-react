2334e1cd42181e7845448416ec24125569cb7816
okmttdhr <okmttdhr@users.noreply.github.com>
2021-04-05 22:45:53 +0900

    Render element

---
+++ my-react.js
@@ -0,0 +1,55 @@
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object"
+          ? child
+          : createTextElement(child)
+      ),
+    },
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
+
+function render(element, container) {
+  const dom =
+    element.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type)
+
+  const isProperty = key => key !== "children"
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name]
+    })
+
+  element.props.children.forEach(child =>
+    render(child, dom)
+  )
+
+  container.appendChild(dom)
+}
+
+const MyReact = {
+  createElement,
+  render,
+}
+
+const text = createTextElement('mine')
+const p = createElement('p', {}, text)
+const element = createElement('div', {}, p)
+
+const container = document.getElementById("root")
+MyReact.render(element, container)
