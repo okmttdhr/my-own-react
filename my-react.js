5fe370339febe336d926fee70e4fc4b6f043ce9d
okmttdhr <okmttdhr@users.noreply.github.com>
2021-04-07 09:41:30 +0900

    Create fiber

--- my-react.js
+++ my-react.js
@@ -22,24 +22,91 @@ function createTextElement(text) {
   }
 }
 
-function render(element, container) {
+function createDom(fiber) {
   const dom =
-    element.type == "TEXT_ELEMENT"
+    fiber.type == "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type)
+      : document.createElement(fiber.type)
 
   const isProperty = key => key !== "children"
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach(name => {
-      dom[name] = element.props[name]
+      dom[name] = fiber.props[name]
     })
 
-  element.props.children.forEach(child =>
-    render(child, dom)
-  )
+  return dom
+}
 
-  container.appendChild(dom)
+let nextUnitOfWork = null
+
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  }
+}
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    console.log(nextUnitOfWork);
+    nextUnitOfWork = performUnitOfWork(
+      nextUnitOfWork
+    )
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+  }
+
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
 }
 
 const MyReact = {
@@ -48,8 +115,12 @@ const MyReact = {
 }
 
 const text = createTextElement('mine')
+const a = createElement('a', {}, text)
 const p = createElement('p', {}, text)
-const element = createElement('div', {}, p)
+const h2 = createElement('h2', {}, text)
+const h1 = createElement('h1', {}, p, a)
+const div = createElement('div', {}, h1, h2)
+const element = createElement('div', {}, div)
 
 const container = document.getElementById("root")
 MyReact.render(element, container)
